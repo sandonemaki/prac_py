6a48312861b9438277ebda7d5690285377247433
sandonemaki <kamuramaki@gmail.com>
2024-03-18 15:01:21 +0900

    グレーの濃淡で作成した画像を保存

--- digitsData1.py
+++ digitsData1.py
@@ -1,7 +1,10 @@
 import sklearn.datasets
+import matplotlib.pyplot as plt
 
 digits = sklearn.datasets.load_digits()
 
-print("データの個数=",len(digits.images))
-print("画像データ=",digits.images[0])
-print("何の数字か=",digits.target[0])
+plt.imshow(digits.images[0], cmap="Greys")
+plt.axis('off') # 軸を非表示にする
+
+plt.savefig('digit_image.png')
+plt.show()
